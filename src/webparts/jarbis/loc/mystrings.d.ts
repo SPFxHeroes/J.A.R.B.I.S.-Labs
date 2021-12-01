f4af6206c906c3c535395d4048b7af2563abb09f
Chris Kent <8364109+thechriskent@users.noreply.github.com>
2021-12-01 15:40:59 -0500

    Initial commit

---
+++ src/webparts/jarbis/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IJarbisWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'JarbisWebPartStrings' {
+  const strings: IJarbisWebPartStrings;
+  export = strings;
+}
